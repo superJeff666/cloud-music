af6f1e2849a93127702a7dce340900bcb21931ed
Jeff <982855239@qq.com>
2020-06-05 21:19:24 +0800

    配置redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { IconStyle } from "./assets/iconfont/iconfont";
 import { GlobalStyle } from "./style";
 import { renderRoutes } from "react-router-config";
 import routes from "./routes/index";
+import store from "./store/index";
 import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
