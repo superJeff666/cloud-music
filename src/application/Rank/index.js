8393a88caee1506dc4143f99de3a287d0d9e401b
Jeff <982855239@qq.com>
2020-12-08 22:17:21 +0800

    feature:排行榜-完成数据部分

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { getRankList } from "./store";
 
 function Rank(props) {
-  return (
-    <div>Rank</div>
-  )
+  const {rankList:list, loading} = props;
+  const {getRankListDispatch} = props;
+  let rankList = list ? list.toJS():[];
+  return <div>Rank</div>;
 }
-export default React.memo(Rank);
+
+const mapStateToProps = (state) => ({
+  rankList: state.getIn(["rank", "rankList"]),
+  loading: state.getIn(["rank", "loading"]),
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getRankListDispatch() {
+      dispatch(getRankList())
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Rank));
